e17edfa313a7862dcc118105be993dd8a2c4a34f
Hydropic <ayman.soultana@ruhr-universitt-bochum.de>
2023-06-03 04:10:22 +0200

    Statistics added selectin projects correctly (Near

--- frontend/src/components/Statistics.jsx
+++ frontend/src/components/Statistics.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   Chart as ChartJS,
@@ -12,10 +12,14 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { faker } from '@faker-js/faker';
+import "./StatisticsStyle.css"
+import axios from "axios"
 
-const Statistics = () => {
+const Statistics = ({accessToken}) => {
+  const [selectedModel1, setSelectedModel1] = useState('');
+  const [selectedModel2, setSelectedModel2] = useState('');
+  const [projects, setProject] = useState([]);
   const [selectedProject, setSelectedProject] = useState('');
-  const [selectedModel, setSelectedModel] = useState('');
 
   ChartJS.register(
     CategoryScale,
@@ -27,15 +31,48 @@ const Statistics = () => {
     Legend
   );
 
+  const fetchProjects = () => {
+
+    axios.get("http://127.0.0.1:8000/get_all_projects_by_user", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        setProject(res.data)
+      }).catch((err) => {
+    });
+
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
   const options = {
     responsive: true,
     plugins: {
       legend: {
         position: 'top',
+        labels: {
+          font: {
+            size: 12, // Decrease legend font size
+          },
+        },
       },
       title: {
         display: true,
-        text: 'Chart.js Line Chart',
+        text: 'Example',
+        font: {
+          size: 18, // Decrease title font size
+        },
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          font: {
+            size: 12, // Decrease y-axis label font size
+          },
+        },
       },
     },
   };
@@ -87,7 +124,8 @@ const Statistics = () => {
         },
         // Add more metrics here
       ],
-    },{
+    },
+    {
       cardTitle: 'Other Metrics',
       data: [
         {
@@ -105,74 +143,103 @@ const Statistics = () => {
 
   const handleProjectChange = (e) => {
     setSelectedProject(e.target.value);
+    console.log(projects)
+    console.log(selectedProject)
   };
 
-  const handleModelChange = (e) => {
-    setSelectedModel(e.target.value);
+  const handleModel1Change = (e) => {
+    setSelectedModel1(e.target.value);
+  };
+
+  const handleModel2Change = (e) => {
+    setSelectedModel2(e.target.value);
   };
 
   return (
-    <div className="container mt-4">
-      <div className="row justify-content-center mb-4">
-        <div className="col-lg-6 col-md-8">
-          <div className="input-group">
-            <select
-              className="form-select"
-              value={selectedProject}
-              onChange={handleProjectChange}
-            >
-              <option value="">Select Project</option>
-              {/* Add options for projects dynamically */}
-              <option value="project1">Project 1</option>
-              <option value="project2">Project 2</option>
-            </select>
-
-            <select
-              className="form-select"
-              value={selectedModel}
-              onChange={handleModelChange}
-            >
-              <option value="">Select Model</option>
-              {/* Add options for models dynamically */}
-              <option value="model1">Model 1</option>
-              <option value="model2">Model 2</option>
-            </select>
-          </div>
-        </div>
-      </div>
-
-      <div className="row justify-content-center mb-4">
-        <div className="col-lg-6 col-md-8">
-          <div className="card bg-light">
-            <div className="card-body">
-              <h4 className="card-title mb-3">Graphs</h4>
-              <Line options={options} data={data} />
+    <div className='scrollable-container'>
+      <div className="container mt-4">
+        <div className="row justify-content-center mb-4">
+          <div className="col-lg-6 col-md-8">
+            <div className="input-group">
+              <select
+                className="form-select"
+                value={selectedProject}
+                onChange={handleProjectChange}
+              >
+                <option value="test">Select Project</option>
+                {projects.map((project) => (
+                <option value={project} key={project.name}>{project.name}</option>
+                ))}
+              </select>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="row justify-content-center">
-        <div className="col-lg-6 col-md-8">
-          <div className="card bg-light">
-            <div className="card-body d-flex flex-row flex-wrap overflow-auto" style={{ maxHeight: '400px' }}>
-              {metrics.map((metricGroup, groupIndex) => (
-                <div key={groupIndex} className="card m-2">
-                  <div className="card-body">
-                    <h5 className="card-title mb-3">{metricGroup.cardTitle}</h5>
-                    {metricGroup.data.map((metric, metricIndex) => (
-                      <div key={metricIndex}>
-                        <h6 className="card-subtitle mb-2 text-muted" style={{ fontSize: '0.8rem' }}>
-                          {metric.label}
-                        </h6>
-                        <p className="card-text" style={{ fontSize: '0.9rem', marginBottom: '0.5rem' }}>
-                          {metric.value}
-                        </p>
-                      </div>
-                    ))}
+        <div className="row justify-content-center mb-4">
+          <div className="col-lg-6 col-md-8">
+            <div className="input-group">
+              <select
+                className="form-select me-2"
+                value={selectedModel1}
+                onChange={handleModel1Change}
+              >
+                <option value="">Select Model 1</option>
+                {/* Add options for models dynamically */}
+                <option value="model1">Model 1</option>
+                <option value="model2">Model 2</option>
+              </select>
+              <span className="input-group-text">&rarr;</span>
+              <select
+                className="form-select ms-2"
+                value={selectedModel2}
+                onChange={handleModel2Change}
+              >
+                <option value="">Select Model 2</option>
+                {/* Add options for models dynamically */}
+                <option value="model1">Model 1</option>
+                <option value="model2">Model 2</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center mb-4">
+          <div className="col-lg-6 col-md-8">
+            <div className="card bg-light">
+              <div className="card-body">
+                <h4 className="card-title mb-3" style={{ fontSize: '1.5rem' }}>
+                  Graphs
+                </h4>
+                <Line options={options} data={data} height={200} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-md-8">
+            <div className="card bg-light">
+              <div className="card-body d-flex flex-row flex-wrap overflow-auto" style={{ maxHeight: '300px' }}>
+                {metrics.map((metricGroup, groupIndex) => (
+                  <div key={groupIndex} className="card m-2" style={{ width: '150px' }}>
+                    <div className="card-body">
+                      <h5 className="card-title mb-3" style={{ fontSize: '1rem' }}>
+                        {metricGroup.cardTitle}
+                      </h5>
+                      {metricGroup.data.map((metric, metricIndex) => (
+                        <div key={metricIndex}>
+                          <h6 className="card-subtitle mb-2 text-muted" style={{ fontSize: '0.8rem' }}>
+                            {metric.label}
+                          </h6>
+                          <p className="card-text" style={{ fontSize: '0.9rem', marginBottom: '0.5rem' }}>
+                            {metric.value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
